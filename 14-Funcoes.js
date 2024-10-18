27cc0896a71ff7971fd54d07e3f425b9c24b7dc6
abraaoteixeira <abraaoteixeira0101@gmail.com>
2024-10-18 20:49:29 -0300

    ad return em functions

--- 14-Funcoes.js
+++ 14-Funcoes.js
@@ -4,3 +4,20 @@ function exibirSoma(x, y)
 }
 
 exibirSoma(1,4)
+
+function exibirSomarComDefault(x=0, y=0)
+{
+    console.log(x+y);
+}
+
+exibirSomarComDefault(5)
+
+//usar retornos para função!
+function somar(x=0, y=0)
+{
+    return x+y
+}
+
+console.log(typeof somar);
+console.log(somar(20,5)); //executa o codigo
+console.log(somar.toString()); //imprime o codigo
