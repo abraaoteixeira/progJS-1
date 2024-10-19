1c51bb9002ec67a8435de2e7790bd5d4ec34e77c
abraaoteixeira <abraaoteixeira0101@gmail.com>
2024-10-18 21:06:47 -0300

    Melhores utilizações de func

--- 11-ReferenciaEValores.js
+++ 11-ReferenciaEValores.js
@@ -5,8 +5,26 @@ const b = a
 console.log('b: ', b)
 //obs: abaixo seria errado, pois é uma atribuição
 //b = {id:19}
-// para altera um valor, acessa-se diretoamento o atributo do objeto
+// para altera um valor, acessa-se diretamente o atributo do objeto
 b.id = 19
 // e agora o que imprimirá?
 console.log('a.id, depois de alterar b.id:', a.id)
 /*imprimiu o valor de b em a. Isso ocore porque ao dizer  */
+
+const imprimirSubtracao = function (a,b)
+{
+    console.log(a - b)
+}
+imprimirSubtracao(5,2)
+
+const subtracao = (a,b) =>
+{
+    return a-b
+}
+console.log('Arrow Function subtração', subtracao(7,8));
+
+const multiplicacao = (x,y) => x*y;
+multiplicacao(3,4);
+
+const saudacao = texto => console.log(texto);
+saudacao('Por hoje é só pessoal')
